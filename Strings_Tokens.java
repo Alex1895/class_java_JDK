d2ee1c4e23f067c1f11aa32e9c29663dfce3a8d6
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-11-05 15:53:43 -0600

    programs in java using strings and regex

---
+++ Strings_Tokens.java
@@ -0,0 +1,30 @@
+package hackerank;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Strings_Tokens {
+	public static void Strings()  {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("write one String: ");
+		String st = sc.nextLine();
+		String [] st1 = st.trim().split("[^a-zA-Z]+");
+		//String [] st1 = st.trim().split("");
+		if(st == null || st.equals("")){
+			//throw new Exception();
+			System.out.println("0");
+		}else if(st.length() > 400000) {
+			return ;
+		}else{
+			
+			System.out.println(st1.length);
+			//System.out.println(st1.);
+			//System.out.println(st1);
+		}
+		sc.close();
+		
+	}
+	public static void main(String[] args){
+		Strings();
+	}
+}
